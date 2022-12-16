3f0337a9cc3ee705b8332896b1062de9cb81f4db
donimhmmdf <donirespect07@gmail.com>
2022-12-16 16:37:32 +0700

    membuat router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./App.css";
-import HelloWorld from "./Components/HelloWorld";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Home from "./Views/Home";
+import About from "./Views/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Header />
-      <HelloWorld name="Asep" />
-      <Footer />
+      <Router>
+        <Header />
+        <div className="p-3">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
