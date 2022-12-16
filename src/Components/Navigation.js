034a58c8ed457d116a99a245a5be56c92d650932
donimhmmdf <donirespect07@gmail.com>
2022-12-16 15:34:13 +0700

    menambah animasi menu

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,23 +1,54 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import { useTransition, animated } from "@react-spring/web";
+import { useTransition, animated } from "react-spring";
 
 function Navigation() {
   const [showMenu, setShowMenu] = useState(false);
-  const [transitions, api] = useTransition(showMenu, () => ({
-    from: { opacity: 0 },
+  const maskTransitions = useTransition(showMenu, {
+    from: { position: "absolute", opacity: 0 },
     enter: { opacity: 1 },
-    leave: { opacity: 1 },
-  }));
-  // menu className="fixed bg-white w-4/5 top-0 left-0 h-full z-50 shadow"
-  // menumask className="bg-black-t-50 w-full h-full z-50 top-0 left-0 fixed"
+    leave: { opacity: 0 },
+  });
+  const menuTransitions = useTransition(showMenu, {
+    from: { opacity: 0, transform: "translateX(-100%)" },
+    enter: { opacity: 1, transform: "translateX(0%)" },
+    leave: { opacity: 0, transform: "translateX(-100%)" },
+  });
 
+  //   className="bg-black-t-50 w-full h-full z-50 top-0 left-0 fixed"
+  //   className="fixed bg-white w-4/5 top-0 left-0 h-full z-50 shadow"
   return (
     <nav>
       <span className="text-xl">
         <FontAwesomeIcon icon={faBars} onClick={() => setShowMenu(!showMenu)} />
       </span>
+      {maskTransitions(
+        (props, item, key) =>
+          item && (
+            <animated.div
+              key={key}
+              style={props}
+              className="bg-black-t-50 w-full h-full z-50 top-0 left-0 fixed"
+              onClick={() => setShowMenu(false)}
+            ></animated.div>
+          )
+      )}
+      {menuTransitions(
+        (props, item, key) =>
+          item && (
+            <animated.div
+              key={key}
+              style={props}
+              className="fixed bg-white w-4/5 top-0 left-0 h-full z-50 shadow p-6"
+            >
+              The Menu
+              <span>
+                <ul>Home</ul>
+              </span>
+            </animated.div>
+          )
+      )}
     </nav>
   );
 }
