2bb4b056bb826fc09f3bf541bdab94d885610669
donimhmmdf <donirespect07@gmail.com>
2022-12-16 13:04:40 +0700

    menambahkan style

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,11 @@
 import React from "react";
-
+import Navigation from "./Navigation";
 function Header() {
-  return <header>AppName</header>;
+  return (
+    <header className="p-3 border-b flex justify-between items-center">
+      <span className="font-bold">App Name</span>
+      <Navigation />
+    </header>
+  );
 }
 export default Header;
