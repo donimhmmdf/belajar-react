0f942f32cdf97c6a57651ba8d1f95ab7e264806f
donimhmmdf <donirespect07@gmail.com>
2022-12-16 16:38:56 +0700

    membuat views

---
+++ src/Views/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2 className="font-bold text-2xl">About</h2>
+      <p>This is about page content</p>
+    </div>
+  );
+}
+export default About;
---
+++ src/Views/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HelloWorld from "../Components/HelloWorld";
+
+function Home() {
+  return (
+    <div>
+      <h2 className="font-bold text-2xl">Home</h2>
+      <HelloWorld name="There" />
+    </div>
+  );
+}
+export default Home;
