93700135400ea5ad44cad5c5b0ab91ff20befd72
donimhmmdf <donirespect07@gmail.com>
2022-12-16 18:00:59 +0700

    fix bug

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-gray-200 text-center absolute bottom-0 w-full text-sm p-3">
+    <footer className="bg-gray-200 text-center fixed bottom-0 w-full text-sm p-3">
       &copy; Copyright 2022
     </footer>
   );
